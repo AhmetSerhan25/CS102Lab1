8137d800139fca77601ffce30c9770247da3849e
AhmetSerhan <159904294+AhmetSerhan25@users.noreply.github.com>
2025-09-26 14:12:16 +0300

    Final Update ArrayOperations.java
    Difference calculator, odd and even index summation methods added.

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -5,7 +5,7 @@ public class ArrayOperations {
   public static int[] generateRandomArray(int size) {
     Random random = new Random();
     int[] arr = new int[size];
-    for(int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
       arr[i] = random.nextInt(101);
     }
     return arr;
@@ -14,10 +14,10 @@ public class ArrayOperations {
   // (c) Finding the array's minimum
   public static int findMin(int[] arr) {
     int min = arr[0];
-    for(int num : arr) {
-        if(num < min) {
-            min = num;
-        }
+    for (int num : arr) {
+      if (num < min) {
+        min = num;
+      }
     }
     return min;
   }
@@ -25,11 +25,55 @@ public class ArrayOperations {
   // (c) Finding the array's maximum
   public static int findMax(int[] arr) {
     int max = arr[0];
-    for(int num : arr) {
-        if(num > max) {
-            max= num;
-        }
+    for (int num : arr) {
+      if (num > max) {
+        max = num;
+      }
     }
     return max;
   }
+
+  // Finding average and returns differences of each number
+  public static int[] differencesFromAverage(int[] sequence) {
+    int sum = 0;
+    for (int i = 0; i < sequence.length; i++) {
+      sum += sequence[i];
+    }
+
+    int average = sum / sequence.length;
+
+    int[] diffs = new int[sequence.length];
+
+    for (int i = 0; i < sequence.length; i++) {
+      diffs[i] = sequence[i] - average;
+    }
+
+    return diffs;
+  }
+
+  // Finding the sum of elements with odd numbered indexes
+  public static void sumOddIndexes(int[] arr) {
+    int oddSum = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      if (i % 2 != 0) {
+        oddSum += arr[i];
+      }
+    }
+
+    System.out.println("Sum of numbers with odd indexes: " + oddSum);
+  }
+
+  public static void sumEvenIndexes(int[] arr) {
+    int evenSum = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      if (i % 2 == 0) {
+        evenSum += arr[i];
+      }
+    }
+
+    System.out.println("Sum of numbers with even indexes: " + evenSum);
+  }
 }
+
